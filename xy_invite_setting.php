90c47aad76ff0b297f9aea11679f5407fbb468a2
unknown <>
2016-01-18 13:57:22 +0800

    增加无限制邀请码、增加批量复制自定义剩余次数显示

--- xy_invite_setting.php
+++ xy_invite_setting.php
@@ -1,40 +1,49 @@
 <?php
 if (!defined('SYSTEM_ROOT')) { die('Insufficient Permissions'); } 
-//参数替换
-function GetData($gs){
-	$tdata = str_ireplace('{年}',date("Y"),$gs);
-	$tdata = str_ireplace('{月}',date("m"),$tdata);
-	$tdata = str_ireplace('{日}',date("d"),$tdata);
-	$tdata = str_ireplace('{时}',date("H"),$tdata);
-	$tdata = str_ireplace('{分}',date("i"),$tdata);
-	$tdata = str_ireplace('{秒}',date("s"),$tdata);
-	preg_match('/{随机\[([1-9]|1[0-9])\]}/',$tdata, $re);
-	if (!empty($re[1])) $tdata = str_ireplace($re[0],getRandStr($re[1]),$tdata);
-	return $tdata; 
+global $m;
+
+//邀请码参数替换
+function getdata_yqm($gs){
+	$data = str_ireplace('{年}',date("Y"),$gs);
+	$data = str_ireplace('{月}',date("m"),$data);
+	$data = str_ireplace('{日}',date("d"),$data);
+	$data = str_ireplace('{时}',date("H"),$data);
+	$data = str_ireplace('{分}',date("i"),$data);
+	$data = str_ireplace('{秒}',date("s"),$data);
+	preg_match('/{随机\[([1-9]|1[0-9])\]}/',$data, $re);
+	if (!empty($re[1])) $data = str_ireplace($re[0],getRandStr($re[1]),$data);
+	return $data; 
 }
 
-global $m;
+//剩余次数格式替换
+function getdata_sy($gs,$num = 0){
+	$gs = !empty($gs) ? $gs : '#剩余{次数}次';
+	$num = !empty($num) ? $num : rand(2,100);
+	$data = str_ireplace('{次数}',$num,$gs);
+	return $data; 
+}
+
 //生成邀请码
 if (isset($_GET['new'])) {
-	$gnum = !empty($_POST['gnum']) ? $_POST['gnum'] : '';
-	$cnum = !empty($_POST['cnum']) ? $_POST['cnum'] : 1;
-	$gs = option::get('xy_invite_gs');
+	$snum = !empty($_POST['snum']) ? $_POST['snum'] : '';
+	$ynum = !empty($_POST['ynum']) ? $_POST['ynum'] : 1;
+	$gs = option::get('xy_invite_gs_yqm');
 	
-	if(empty($gnum)){$emsg = '生成数量不能为空！';}
-	elseif($gnum <= 0){$emsg = '生成数量不能小于1！';}
-	elseif($gnum > 100){$emsg = '生成数量不能超过100！';}
-	elseif(empty($cnum)){$emsg = '使用次数不能为空！';}
-	elseif($cnum <= 0){$emsg = '使用次数不能小于1！';}
-	elseif($cnum > 99999){$emsg = '使用次数不能大于99999！';}
+	if(empty($snum)){$emsg = '生成数量不能为空！';}
+	elseif($snum <= 0){$emsg = '生成数量不能小于1！';}
+	elseif($snum > 100){$emsg = '生成数量不能超过100！';}
+	elseif(empty($ynum)){$emsg = '使用次数不能为空！';}
+	elseif($ynum < -1){$emsg = '使用次数不能小于 -1， -1表示无限。';}
+	elseif($ynum > 99999){$emsg = '使用次数不能大于99999！';}
 	elseif(empty($gs)){$emsg = '没有设置邀请码格式，请先设置邀请码生成格式！';$page=1;}
-	elseif(strlen(GetData($gs)) > 100){$emsg = '邀请码长度超出限制，请重新设置！请控制在100字符内！';$page=1;}
+	elseif(strlen(getdata_yqm($gs)) > 100){$emsg = '邀请码长度超出限制，请重新设置！请控制在100字符内！';$page=1;}
 	if(!empty($emsg)) {
 		$page = !empty($page) ? $page : 3;
 		ReDirect(SYSTEM_URL.'index.php?mod=admin:setplug&plug=xy_invite&page='.$page.'&error_msg='.$emsg);
 	}
-	for($i=0;$i<$gnum;$i++){
-		$yqm = sqladds(GetData($gs));
-		$sql = "INSERT INTO `".DB_NAME."`.`".DB_PREFIX."xy_invite` (`code`, `num`) VALUES ('{$yqm}', '{$cnum}');";
+	for($i=0;$i<$snum;$i++){
+		$yqm = sqladds(getdata_yqm($gs));
+		$sql = "INSERT INTO `".DB_NAME."`.`".DB_PREFIX."xy_invite` (`code`, `num`) VALUES ('{$yqm}', '{$ynum}');";
 		$m->query($sql);
 	}
 	ReDirect(SYSTEM_URL.'index.php?mod=admin:setplug&plug=xy_invite&page=2&msg=邀请码生成完毕。');
@@ -52,12 +61,19 @@ if (isset($_GET['del']) && !empty($_GET['id'])) {
 }
 //基本设置
 if (isset($_GET['set'])) {
-	if(!empty($_POST['gs'])) {
-		if(strlen(GetData($_POST['gs'])) > 100) {
+	if(!empty($_POST['gs_yqm'])) {
+		if(strlen(getdata_yqm($_POST['gs_yqm'])) > 100) {
 			ReDirect(SYSTEM_URL.'index.php?mod=admin:setplug&plug=xy_invite&error_msg=邀请码长度超出限制！请控制在100字符内！');
 		} else {
-			option::set('xy_invite_gs',$_POST['gs']);
-			ReDirect(SYSTEM_URL.'index.php?mod=admin:setplug&plug=xy_invite&msg=邀请码格式已保存！');
+			$show_num = !empty($_POST['show_num']) ? 1 : 0;
+			$hide_num_1 = !empty($_POST['hide_num_1']) ? 1 : 0;
+			$hide_num_wx = !empty($_POST['hide_num_wx']) ? 1 : 0;
+			option::set('xy_invite_gs_yqm',$_POST['gs_yqm']);
+			option::set('xy_invite_gs_sy',$_POST['gs_sy']);
+			option::set('xy_invite_shownum',$show_num);
+			option::set('xy_invite_hidenum_1',$hide_num_1);
+			option::set('xy_invite_hidenum_wx',$hide_num_wx);
+			ReDirect(SYSTEM_URL.'index.php?mod=admin:setplug&plug=xy_invite&msg=设置已保存！');
 		}
 	} else {
 		ReDirect(SYSTEM_URL.'index.php?mod=admin:setplug&plug=xy_invite&error_msg=邀请码格式不能为空！');
@@ -92,27 +108,51 @@ if (!option::get('yr_reg')) {echo '<div class="alert alert-warning alert-dismiss
 <div class="tab-pane fade in active" id="adminid">
 	<a name="#adminid"></a>
 	<form action="index.php?mod=admin:setplug&plug=xy_invite&set" method="post">
-		<div class="input-group">
-			<span class="input-group-addon">邀请码格式</span>
-			<input type="text" name="gs" class="form-control" value="<?php echo option::get('xy_invite_gs');?>" placeholder="学园-{年}-{随机[18]}" required />
-		</div><br>
-		<div class="input-group">
-			<span class="input-group-addon">邀请码预览</span>
-			<input type="text" class="form-control" value="<?php echo GetData(option::get('xy_invite_gs'));?>" placeholder="警告，当前没有设置格式，将会生成失败" disabled />
-		</div><br>
+		<div class="table-responsive">
+			<table class="table table-hover"><tbody>
+				<tr><td>
+                	<div class="input-group">
+						<span class="input-group-addon">邀请码格式</span>
+						<input type="text" name="gs_yqm" class="form-control" value="<?php echo option::get('xy_invite_gs_yqm');?>" placeholder="参考格式：学园-{年}-{随机[18]}" required/>
+					</div><br/>
+					<div class="input-group">
+						<span class="input-group-addon">邀请码预览</span>
+						<input type="text" class="form-control" value="<?php echo getdata_yqm(option::get('xy_invite_gs_yqm'));?>" placeholder="警告，当前没有设置格式，将会生成失败" disabled/>
+					</div>
+				</td></tr>
+				<tr><td>
+                	<div class="input-group">
+						<label><input type="checkbox" name="show_num" value="1" <?php echo option::get('xy_invite_shownum') == 1 ? 'checked' : ''; ?>> 批量复制时输出剩余次数</label>&nbsp;&nbsp;&nbsp;&nbsp;
+						<label><input type="checkbox" name="hide_num_1" value="1" <?php echo option::get('xy_invite_hidenum_1') == 1 ? 'checked' : ''; ?>> 剩余次数为1时不输出次数</label>&nbsp;&nbsp;&nbsp;&nbsp;
+						<label><input type="checkbox" name="hide_num_wx" value="1" <?php echo option::get('xy_invite_hidenum_wx') == 1 ? 'checked' : ''; ?>> 无限次邀请码不输出次数</label>
+					</div><br/>
+					<div class="input-group">
+						<span class="input-group-addon">剩余次数格式</span>
+						<input type="text" name="gs_sy" class="form-control" value="<?php echo option::get('xy_invite_gs_sy');?>" placeholder="在此输入剩余格式，留空将默认：#剩余{次数}次"/>
+					</div><br/>
+					<div class="input-group">
+						<span class="input-group-addon">导出格式预览</span>
+						<input type="text" class="form-control" value="<?php echo getdata_yqm(option::get('xy_invite_gs_yqm')).getdata_sy(option::get('xy_invite_gs_sy'));?>" placeholder="警告，当前没有设置格式，将会生成失败" disabled />
+					</div>
+				</td></tr>
+			</tbody></table>
+		</div>
 		<button type="submit" class="btn btn-success">保存</button>
 	</form><br/><br/><br/>
 
 	<div class="panel panel-default">
-		<div class="panel-heading" onClick="$('#win_bduss').fadeToggle();"><h3 class="panel-title"><span class="glyphicon glyphicon-chevron-down"></span> 邀请码格式可用参数</h3></div>
+		<div class="panel-heading" onClick="$('#win_bduss').fadeToggle();"><h3 class="panel-title"><span class="glyphicon glyphicon-chevron-down"></span> 格式参数简介</h3></div>
 		<div class="panel-body" id="win_bduss">
+        	<h3 style="margin-top:0;">邀请码格式参数</h3>
 			<p>1、<b>{年}</b> 该参数会被替换成当前日期的年份。</p>
 			<p>2、<b>{月}</b> 该参数会被替换成当前日期的月份。</p>
 			<p>3、<b>{日}</b> 该参数会被替换成当天日期。</p>
             <p>4、<b>{时}</b> 该参数会被替换成当前时间的小时。</p>
             <p>5、<b>{分}</b> 该参数会被替换成当前时间的分钟。</p>
             <p>6、<b>{秒}</b> 该参数会被替换成当前时间的秒钟。</p>
-            <p>7、<b>{随机[n]}</b> 该参数会被替换成随机的字符串；n表示字符串长度，数字范围限制在（1-19），超过范围无效！</p>
+            <p>7、<b>{随机[n]}</b> 该参数会被替换成随机的字符串；n表示字符串长度，数字范围限制在（1-19），超过范围无效！</p><br/>
+            <h3 style="margin-top:0;">剩余次数格式参数</h3>
+            <p><b>{次数}</b> 该参数会被替换成当前邀请码剩余次数。</p>
 		</div>
 	</div>
 </div>
@@ -124,18 +164,32 @@ if (!option::get('yr_reg')) {echo '<div class="alert alert-warning alert-dismiss
 	<table class="table table-hover">
 		<tbody>
 			<tr>
-				<th>ID</th>
+				<th>#</th>
 				<th>邀请码<?php $s = $m->query("SELECT * FROM `".DB_NAME."`.`".DB_PREFIX."xy_invite`");
-				if($s->num_rows > 0){echo '&nbsp;&nbsp;<button type="button" class="btn btn-xs btn-info" data-toggle="modal" data-target="#copycode">批量复制</button>';}?></th>
+				if($s->num_rows > 0){$gs_sy = option::get('xy_invite_gs_sy');echo '&nbsp;&nbsp;<button type="button" class="btn btn-xs btn-info" data-toggle="modal" data-target="#copycode">批量复制</button>';}?></th>
 				<th>剩余次数</th>
 				<th>操作</th>
 			</tr>
 <?php
 	while ($x = $m->fetch_array($s)) {
-		$ydata .= '<tr><td>'.$x['id'].'</td><td>'.$x['code'].'</td><td>'.$x['num'].'</td><td><button type="button" onclick="{if(confirm(\'确定要删除这个邀请码吗？一旦删除无法恢复！\')){window.location = \'./index.php?mod=admin:setplug&plug=xy_invite&id='.$x['id'].'&del\';}return false;};" class="btn btn-sm btn-danger">删除</button></td></tr>';
-		$copyyqm .= $x['code'].PHP_EOL;
+		$x_num = $x['num'] == -1 ? '无限' : $x['num'];
+		if($x_num == 1) {$ys = 'default';}
+		elseif ($x_num > 1) {$ys = 'primary';}
+		else {$ys = 'success';}
+
+		$data .= '<tr><td>'.$x['id'].'</td><td>'.$x['code'].'</td><td><span class="label label-'.$ys.'">'.$x_num.'</span></td><td><button type="button" onclick="{if(confirm(\'确定要删除这个邀请码吗？一旦删除无法恢复！\')){window.location = \'./index.php?mod=admin:setplug&plug=xy_invite&id='.$x['id'].'&del\';}return false;};" class="btn btn-sm btn-danger">删除</button></td></tr>';
+		if (option::get('xy_invite_shownum') == 1) {
+			if ($x_num == 1) {
+				$sy_num = option::get('xy_invite_hidenum_1') != 1 ? getdata_sy($gs_sy,$x_num) : '';
+			} elseif ($x_num == '无限') {
+				$sy_num = option::get('xy_invite_hidenum_wx') != 1 ? getdata_sy($gs_sy,$x_num) : '';
+			} else {
+				$sy_num = getdata_sy($sygs,$x_num);
+			}
+		}
+		$yqm .= $x['code'].$sy_num.PHP_EOL;
 	}
-	echo $ydata;
+	echo $data;
 ?>
 		</tbody>
 	</table>
@@ -147,11 +201,11 @@ if (!option::get('yr_reg')) {echo '<div class="alert alert-warning alert-dismiss
 	<form action="index.php?mod=admin:setplug&plug=xy_invite&new" method="post">
 		<div class="input-group">
 			<span class="input-group-addon">生成数量</span>
-			<input type="number" name="gnum" min="1" max="100" class="form-control" placeholder="请在此输入生成数量，为空将生成失败" value="10" required />
+			<input type="number" name="snum" min="1" max="100" class="form-control" placeholder="请在此输入生成数量，为空将生成失败" value="10" required />
 		</div><br>
 		<div class="input-group">
 			<span class="input-group-addon">有效次数</span>
-			<input type="number" name="cnum" max="99999" class="form-control" placeholder="请在此输入邀请码的使用次数，留空将默认一次性。" />
+			<input type="number" name="ynum" min="-1" max="99999" class="form-control" placeholder="邀请码有效次数，留空将默认一次性，-1 为无限。" />
 		</div><br>
 		<button type="submit" onClick="if(!confirm('确定要生成邀请码吗？')){return false;};" class="btn btn-success">生成邀请码</button>&nbsp;&nbsp;<button type="button" onClick="{if(confirm('确定要清空邀请码吗？一旦清空无法恢复！！')){window.location = './index.php?mod=admin:setplug&plug=xy_invite&delete';}return false;};" class="btn btn-danger">清空所有邀请码</button>
 	</form>
@@ -167,7 +221,7 @@ if (!option::get('yr_reg')) {echo '<div class="alert alert-warning alert-dismiss
         <h4 class="modal-title">请手动复制下面的邀请码</h4>
       </div>
       <div class="modal-body">
-		<textarea id="invitecode" class="form-control"><?php echo $copyyqm;?></textarea>
+		<textarea id="invitecode" class="form-control"><?php echo $yqm;?></textarea>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-primary" data-dismiss="modal">确定</button>
