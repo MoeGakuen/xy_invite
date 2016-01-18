90abbf4a0a002730967065f3174cd64b8bf5b092
unknown <>
2016-01-18 13:55:00 +0800

    优化插件卸载清理。

--- xy_invite_callback.php
+++ xy_invite_callback.php
@@ -7,14 +7,14 @@ function callback_install() {
 	`num` int(5) DEFAULT NULL,
 	PRIMARY KEY (`id`)
 	) ENGINE=MyISAM DEFAULT CHARSET=utf8;');
-	option::set('xy_invite_gs','学园-{年}-{随机[19]}');
+	option::set('xy_invite_gs_yqm','学园-{年}-{随机[19]}');
 	$m->query("UPDATE `".DB_NAME."`.`".DB_PREFIX."plugins` SET `order`='50' WHERE `name`='xy_invite';");//兼容某些插件，放后加载。
 }
 
 function callback_remove() {
 	global $m;
 	option::set('yr_reg','');
-	$m->query("DELETE FROM `".DB_NAME."`.`".DB_PREFIX."options` WHERE `name` = 'xy_invite_gs'");
+	$m->query("DELETE FROM `".DB_NAME."`.`".DB_PREFIX."options` WHERE `name` LIKE '%xy_invite_%'");
 	$m->query('DROP TABLE `'.DB_NAME.'`.`'.DB_PREFIX.'xy_invite`');
 }
 
